5a91879ec90b9b019c8095b469dca151c02131d1
Courtney Sims <csims@cloudflare.com>
2025-01-10 13:53:26 -0600

    fixed query math

--- src/app/lib/data.tsx
+++ src/app/lib/data.tsx
@@ -5,11 +5,13 @@ export async function fetchReadyCats(): Promise<string[]>{
     const myDB = (await getCloudflareContext()).env.PROD_DB;
     
     const data = await myDB.prepare(
-        `SELECT CatName FROM Cats WHERE LastDonated>date('now','-8 weeks') or LastDonated is NULL;`
+        `SELECT CatName FROM Cats WHERE LastDonated<date('now', '-56 days') or LastDonated is NULL;`
     ).run();
     console.log(data);
     const readyCats: string[] = []
     for (const row in data.results){
+        //TODO: don't ignore. parse into type
+        // @ts-ignore
         readyCats.push(data.results[row]["CatName"])
     }
     return readyCats
