bb8def5fb3906819eaa28fe8fc835876c62c5e4e
Courtney <courtsims09@hotmail.com>
2025-05-29 22:19:15 -0500

    Added zod

--- src/app/lib/data.tsx
+++ src/app/lib/data.tsx
@@ -1,4 +1,16 @@
 import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { z } from "zod/v4";
+
+const Cat = z.object({
+    //should this be a uuid instead? when and why do those matter?
+    CatId: z.number(),
+    CatName: z.string(),
+    LastDonated: z.date(),
+});
+
+type Cat = z.infer<typeof Cat>;
+
+const CatGetResponse = z.record(z.string(), Cat)
 
 
 export async function fetchReadyCats(): Promise<string[]>{
@@ -8,11 +20,16 @@ export async function fetchReadyCats(): Promise<string[]>{
         `SELECT CatName FROM Cats WHERE LastDonated<date('now', '-56 days') or LastDonated is NULL;`
     ).run();
     console.log(data);
+
+    const validatedResp = CatGetResponse.safeParse(data.results);
+    if (!validatedResp.success) {
+        console.log(`Ready cats parsing error: ${validatedResp.error}`);
+        return [];
+    }
+    
     const readyCats: string[] = []
-    for (const row in data.results){
-        //TODO: don't ignore. parse into type
-        // @ts-ignore
-        readyCats.push(data.results[row]["CatName"])
+    for (const row in validatedResp.data){
+        readyCats.push(validatedResp.data[row]["CatName"])
     }
     return readyCats
 }
